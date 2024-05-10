6feaa63af69ded3da950b635690bc8d5ea1b0350
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-09 21:56:09 -0300

    test: create UpperHeader tests

---
+++ __tests__/components/Header.jest.tsx
@@ -0,0 +1,30 @@
+import { UpperHeader } from '@/components';
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react';
+
+describe("<UpperHeader />", () => {
+    const renderComponent = () => render(<UpperHeader />);
+
+    it("should render the UpperHeader component", () => {
+      renderComponent();
+      const headerElement = screen.getByRole('banner');
+      expect(headerElement).toBeInTheDocument();
+    });
+
+    it("should not be visible on mobile devices", () => {
+      renderComponent();
+      const headerElement = screen.getByRole('banner');
+
+      expect(headerElement).toHaveClass('hidden');
+    });
+    
+    it("should be visible on desktop devices", () => {
+      renderComponent();
+      const headerElement = screen.getByRole('banner');
+      
+      expect(headerElement).toBeInTheDocument();
+      expect(headerElement).toBeVisible();
+      expect(headerElement).toHaveClass('md:flex');
+    });
+  });
+  
