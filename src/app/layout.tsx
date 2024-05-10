ee9acc58b566dc0bbb30a42ebea1527033d44773
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-10 19:33:46 -0300

    feat: implement UpperHeader to screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { UpperHeader } from "@/components";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} text-[#333333]`}>
+        <UpperHeader />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
