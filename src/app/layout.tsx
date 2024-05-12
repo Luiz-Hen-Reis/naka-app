7a42939c40f5b12be9f38bfab5e9ca45ea0f117d
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-12 13:20:10 -0300

    chore: wrap app with ModalsProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { UpperHeader } from "@/components";
+import { ModalsProvider } from "@/contexts/modalsContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={`${inter.className} text-[#333333]`}>
-        <UpperHeader />
-        <main className="flex max-w-[1440px] lg:px-10 lg:mt-10 mx-auto">{children}</main>
+        <ModalsProvider>
+          <UpperHeader />
+          <main className="flex flex-col max-w-[1440px] lg:px-10 lg:mt-10 mx-auto">{children}</main>
+        </ModalsProvider>
       </body>
     </html>
   );
