4f1695e79f0bce3273c56e726276fbbdf34e3381
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-13 20:56:57 -0300

    chore: add Providers component to index components file

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import MainHeader from "./MainHeader";
 import UpperHeader from "./UpperHeader";
 import SearchItemsBar from "./SearchItemsBar";
 import DeliveryModal from "./modals/DeliveryMethodModal";
+import Providers from "./Providers";
 
-export { UpperHeader, MainHeader, SearchItemsBar, DeliveryModal };
+export { UpperHeader, MainHeader, SearchItemsBar, DeliveryModal, Providers };
