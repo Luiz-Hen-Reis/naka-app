e9cf279ba14b340778b2b5e2e5705ab1574be25b
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-17 20:58:01 -0300

    chore: create icons array util

---
+++ src/utils/iconsArray.ts
@@ -0,0 +1,18 @@
+export const iconsArray = [
+    {
+        path: "/assets/icons/house.svg",
+        iconName: "Inicio",
+    },
+    {
+        path: "/assets/icons/orders.svg",
+        iconName: "Pedidos",
+    },
+    {
+        path: "/assets/icons/bag_black.svg",
+        iconName: "Bolsa"
+    },
+    {
+        path: "/assets/icons/profile.svg",
+        iconName: "Perfil",
+    }
+];
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 import { API_URI } from "./API_URI";
+import { iconsArray } from "./iconsArray";
 
-export { API_URI };
+export { API_URI, iconsArray };
