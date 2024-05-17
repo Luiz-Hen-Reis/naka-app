58c495001d55a6f8fcc5a49552224746a324fda7
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-16 21:08:37 -0300

    feat: add categories and products to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
-import { MainHeader, SearchItemsBar } from "@/components";
+"use client";
+
+import { CategoryItem, CategoryItemSkeleton, MainHeader, SearchItemsBar } from "@/components";
+import { useGetProducts } from "@/hooks";
 
 export default function Home() {
+  const { data, isLoading } = useGetProducts();
+
   return (
     <>
       <MainHeader />
       <div className="app-container">
         <SearchItemsBar />
+        {isLoading &&
+          <CategoryItemSkeleton />
+        }
+        {!isLoading &&
+          <>
+            {data?.categories.map((category) => (
+            <CategoryItem {...category} key={category.category_id} />
+          ))}
+          </>
+        }
       </div>
     </>
   )
