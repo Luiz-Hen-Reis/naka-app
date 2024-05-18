c7f26fe5b79de882611052f93b434bbb1159c29b
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-18 19:07:21 -0300

    feat: add SearchProvider to Providers component

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,6 +3,7 @@
 import { ReactNode } from "react"
 import { ModalsProvider } from "@/contexts/modalsContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SearchProvider } from "@/contexts/searchContext";
 
 type Props = {
     children: ReactNode;
@@ -13,11 +14,13 @@ const queryClient = new QueryClient();
 export default function Providers({ children }: Props) {
   return (
     <>
-    <ModalsProvider>
-        <QueryClientProvider client={queryClient}>
-            {children}
-        </QueryClientProvider>
-    </ModalsProvider>
+      <SearchProvider>
+        <ModalsProvider>
+            <QueryClientProvider client={queryClient}>
+                {children}
+            </QueryClientProvider>
+        </ModalsProvider>
+      </SearchProvider>
     </>
   )
 }
