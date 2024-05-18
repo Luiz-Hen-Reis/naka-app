8967255c8e7513467bf497e2f1206c4c552c6d08
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-18 19:10:06 -0300

    chore: provide value and onChange to InputSearchItems

--- src/components/ui/InputSearchItems.tsx
+++ src/components/ui/InputSearchItems.tsx
@@ -1,8 +1,13 @@
+"use client";
+
+import { useSearchContext } from "@/hooks";
 import { InputHTMLAttributes } from "react";
 
 interface InputSearchItemsProps extends InputHTMLAttributes<HTMLInputElement> {};
 
 export default function InputSearchItems({  }: InputSearchItemsProps) {
+    const { search, handleSearch } = useSearchContext();
+
   return (
         <div className="rounded-md flex-1 flex items-center lg:gap-4 bg-[#e8e8e8] border-2 h-11 lg:h-16 lg:bg-transparent lg:border-2 lg:p-4">
             <img src="/assets/icons/search.svg" alt="search" className="w-4 h-4" />
@@ -10,6 +15,8 @@ export default function InputSearchItems({  }: InputSearchItemsProps) {
                 type="text"
                 placeholder="Buscar en el menú"
                 className="flex-1 h-full outline-none bg-transparent ml-2"
+                value={search}
+                onChange={handleSearch}
             />
         </div>
     )
