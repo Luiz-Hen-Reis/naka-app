a513b34466cb5f8d8970900bd195f74392c6ce37
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-19 08:55:09 -0300

    fix: provide key prop to element

--- src/components/FooterNav.tsx
+++ src/components/FooterNav.tsx
@@ -5,7 +5,7 @@ export default function FooterNav() {
     <footer className="fixed bottom-0 w-full bg-white pt-3 border-t-2 z-10 md:hidden">
       <ul className="flex justify-evenly items-center">
         {iconsArray.map((icon) => (
-            <li className="w-5 cursor-pointer flex flex-col items-center">
+            <li className="w-5 cursor-pointer flex flex-col items-center" key={icon.iconName}>
                 <img src={icon.path} alt={icon.iconName} />
                 <p className="font-extralight text-sm">{icon.iconName}</p>
             </li>
