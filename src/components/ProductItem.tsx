a160a9b609b6483ec665b6b06b4a6fd9792ae6c2
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-20 20:56:39 -0300

    feat: add ProductModal and logic to handle it

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,22 +1,36 @@
+'use client';
+
 import { Product } from "@/types";
 import { API_URI } from "@/helpers";
+import ProductModal from "./modals/ProductModal";
+import { useState } from "react";
 
 interface ProductItemProps extends Product {}
 
-export default function ProductItem({ name, price, description, image_url }: ProductItemProps) {
-  return (
-    <div className='flex justify-between items-start gap-2 py-4 md:px-4 md:border rounded-sm md:shadow-sm cursor-pointer'>
-        <div className='flex flex-col'>
-            <h3 className='font-bold pb-4 md:text-lg'>{name}</h3>
-            <p className='text-xs text-[#717171] pb-5'>{description}</p>
-            <span>$ {price}</span>
-        </div>
+export default function ProductItem({ name, price, description, image_url, id }: ProductItemProps) {
+  const [isProductModalOpen, setIsProductModalOpen] = useState<boolean>(false);
 
-        <div>
-            <div className='w-24 h-16'>
-                <img src={`${API_URI}${image_url}`} alt={name} />
-            </div>
+  const handleOpenProductModal = () => setIsProductModalOpen(true);
+  const handleCloseProductModal = () => setIsProductModalOpen(false);
+
+  const productProps = { id, name, price, image_url, description, onClose: handleCloseProductModal };
+
+  return (
+      <>
+        <div className='flex justify-between items-start gap-2 py-4 md:px-4 md:border rounded-sm md:shadow-sm cursor-pointer hover:border-[#717171] transition' onClick={handleOpenProductModal}>
+          <div className='flex flex-col'>
+              <h3 className='font-bold pb-4 md:text-lg'>{name}</h3>
+              <p className='text-xs text-[#717171] pb-5'>{description}</p>
+              <span>$ {price}</span>
+          </div>
+
+          <div>
+              <div className='w-24 h-16'>
+                  <img src={`${API_URI}${image_url}`} alt={name} className="object-cover" />
+              </div>
+          </div>
         </div>
-</div>
+        {isProductModalOpen && <ProductModal {...productProps} />}
+      </>
   )
 }
