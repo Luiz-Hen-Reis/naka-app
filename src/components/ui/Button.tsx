961d6c6e1914c79336f7bde203f71f7fc2983044
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-21 21:01:17 -0300

    refactor: change extend type and provide all button props tu Button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,13 +1,15 @@
 import { cn } from "@/libs/utils";
 import { ReactNode } from "react"
 
-interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode;
 }
 
-export default function Button({ children, className }: ButtonProps) {
+export default function Button({ children, className, ...props }: ButtonProps) {
   return (
-    <button className={cn('text-white secondary-bg-color my-2 py-4 px-2 rounded-sm shadow-md', className)}>
+    <button  className={cn('text-white secondary-bg-color my-2 py-4 px-2 rounded-sm shadow-md', className)} 
+    {...props}
+    >
         {children}
     </button>
   )
