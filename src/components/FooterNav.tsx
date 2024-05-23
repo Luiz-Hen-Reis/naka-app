291ba3440bcf72d9e72179c8490597643bf08bee
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-23 20:51:19 -0300

    style: update FooterNav z-index value

--- src/components/FooterNav.tsx
+++ src/components/FooterNav.tsx
@@ -2,7 +2,7 @@ import { iconsArray } from "@/helpers";
 
 export default function FooterNav() {
   return (
-    <footer className="fixed bottom-0 w-full bg-white pt-3 border-t-2 z-10 md:hidden">
+    <footer className="fixed bottom-0 w-full bg-white pt-3 border-t-2 z-40 md:hidden">
       <ul className="flex justify-evenly items-center">
         {iconsArray.map((icon) => (
             <li className="w-5 cursor-pointer flex flex-col items-center" key={icon.iconName}>
