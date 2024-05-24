82471b015a7ca2bfe34dbaa72e0a0b58fdf26f12
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-24 20:46:00 -0300

    refactor: add bag items counter to FooterNav

--- src/components/FooterNav.tsx
+++ src/components/FooterNav.tsx
@@ -1,8 +1,24 @@
+"use client";
+
 import { iconsArray } from "@/helpers";
+import { useAppContext, useBag } from "@/hooks";
 
 export default function FooterNav() {
+  const { bag } = useBag();
+  const { handleToggleShoppingBag } = useAppContext();
+
   return (
     <footer className="fixed bottom-0 w-full bg-white pt-3 border-t-2 z-40 md:hidden">
+
+      <div className={`flex justify-around items-center secondary-bg-color text-white py-4 text-xs mb-2 shadow-md ${bag.length > 0 ? '' : 'hidden'}`} onClick={handleToggleShoppingBag}>
+        <div className="relative">
+          <img src="/assets/icons/bag_white.svg" alt="bag" />
+          <span className="absolute top-0 right-0 w-3 h-3 rounded-full bg-[#EEEEEE] secondary-text-color flex items-center justify-center">{bag.length}</span>
+        </div>
+        <p>Ver bolsa</p>
+        <span>$ 0.00</span>
+      </div>
+
       <ul className="flex justify-evenly items-center">
         {iconsArray.map((icon) => (
             <li className="w-5 cursor-pointer flex flex-col items-center" key={icon.iconName}>
