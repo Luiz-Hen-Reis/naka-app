f493f43b066911490c576237f899e3b60c47f349
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-25 09:36:55 -0300

    import useBag hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,9 @@
 import { useGetProducts } from "./useGetProducts";
 import { useAppContext } from "./useAppContext";
+import useBag from "./useBag";
 
 export { 
     useAppContext, 
     useGetProducts, 
+    useBag
  };
