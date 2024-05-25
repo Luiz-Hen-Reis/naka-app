237c71dd9bd8e34819dca701a37be389f473a1eb
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-25 09:34:38 -0300

    refactor: add Button and SelectQuantity

--- src/components/modals/ProductModal.tsx
+++ src/components/modals/ProductModal.tsx
@@ -3,7 +3,8 @@
 import { BagProduct, Product } from "@/types";
 import { API_URI } from "@/helpers";
 import { useState } from "react";
-import { Button } from "../ui";
+import { Button, CloseButton, SelectQuantity } from "../ui";
+import { useBag } from "@/hooks";
 
 interface ProductModalProps extends Product {
   onClose: () => void;
@@ -11,23 +12,23 @@ interface ProductModalProps extends Product {
 
 export default function ProductModal({ id, name, price, description, image_url, onClose }: ProductModalProps) {
     const [quantity, setQuantity] = useState(1);
+    const { addItemToBag } = useBag();
 
-    const increaseQuantity = () => setQuantity(quantity + 1);
+    const increaseQuantity = () => setQuantity(quantity  === 8 ? quantity : quantity + 1);
     const decreaseQuantity = () => setQuantity(quantity > 1 ? quantity - 1 : 1);
 
     const chosenProduct: BagProduct  = { id, name, price, description, image_url, quantity };
 
-    const handleSelectProduct = () => {
-      console.log(chosenProduct);
+    const handleAddProductToBag = () => {
+      addItemToBag(chosenProduct);
       onClose();
     }
-    
   
     return (
         <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex justify-center items-end md:items-center z-50">
-          <div className="relative bg-white rounded-lg shadow-lg w-full h-full md:max-w-[1200px] md:h-[580px] flex flex-col md:flex-row md:justify-between md:py-8 md:px-2">
+          <div className="relative bg-white rounded-lg shadow-lg w-full h-full md:max-w-[1200px] md:h-[580px] flex flex-col md:flex-row md:justify-between md:py-8 md:px-2 z-[99]">
 
-              <button className="text-3xl text-left p-2 md:absolute md:top-3 md:right-3" onClick={onClose}>&times;</button>
+              <CloseButton className="md:absolute md:top-3 md:right-3" onClick={onClose} />
 
               <div className="flex flex-col md:flex-row gap-4 md:gap-0 h-full w-full">
                 <div className="md:w-1/2 h-44 md:h-full flex-shrink-0">
@@ -40,24 +41,16 @@ export default function ProductModal({ id, name, price, description, image_url, 
                       <p>{description}</p>
                     </div>
 
-                    <div className="flex flex-col gap-4 md:flex-row justify-end">
-
-                    <div className="flex items-center space-x-4 border w-fit p-2">
-                      <button 
-                        onClick={decreaseQuantity} 
-                        className={`px-3 py-1 rounded text-lg ${quantity !== 1 ? 'secondary-text-color' : 'cursor-not-allowed'}`}
-                        disabled={quantity === 1}
-                        >
-                        -
-                      </button>
-                      <span className="font-bold text-xl w-6 h-6 text-center">{quantity}</span>
-                      <button 
-                        onClick={increaseQuantity} 
-                        className="px-3 py-1 rounded text-lg secondary-text-color">
-                        &#43;
-                      </button>
-                    </div>
-                      <Button className="md:flex md:justify-between md:items-center text-lg md:w-64" onClick={handleSelectProduct}>Añadir <span>$ {price}</span></Button>
+                    <div className="flex gap-4 justify-center mb-2 md:justify-end">
+                      <SelectQuantity quantity={quantity} increaseQuantity={increaseQuantity} decreaseQuantity={decreaseQuantity}
+                        className="p-0"
+                        buttonClassName="px-3 py-0"
+                        quantityCounterClassName="text-md"
+                      />
+                    
+                      <Button className="flex-1 text-sm md:flex md:justify-between md:items-center md:text-base md:px-4" onClick={handleAddProductToBag}>
+                        Añadir <span>$ {quantity * price}</span>
+                      </Button>
                     </div>
                 </div>
                 
