cab37d4dece0b9a165b619e32e88957ddf75231f
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-25 09:36:28 -0300

    feat: create EmptyBag component

---
+++ src/components/ui/EmptyBag.tsx
@@ -0,0 +1,11 @@
+export default function EmptyBag() {
+  return (
+    <div className="flex-1 flex flex-col justify-center items-center">
+        <div className="w-20 h-w-20 mt-[-200px]">
+            <img src="/assets/icons/bag_2.svg" alt="empty bag" />
+        </div>
+
+        <span>Tu bolsa esta vazia</span>
+    </div>
+  )
+}
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,10 +2,12 @@ import InputSearchItems from "./InputSearchItems";
 import Button from "./Button";
 import CloseButton from "./CloseButton";
 import SelectQuantity from "./SelectQuantity";
+import EmptyBag from "./EmptyBag";
 
 export { 
     InputSearchItems,
     Button,
     CloseButton,
-    SelectQuantity
+    SelectQuantity,
+    EmptyBag
  };
