3f7f785dd9a922bad68bca2ba7287d9719e93cbf
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-25 09:35:45 -0300

    feat: create bag context value

--- src/contexts/appContext.tsx
+++ src/contexts/appContext.tsx
@@ -1,33 +1,52 @@
 "use client";
 
-import { ChangeEvent, MutableRefObject, ReactNode, createContext, useEffect, useRef, useState } from "react";
+import { BagProduct } from "@/types";
+import { 
+    ChangeEvent,
+    MutableRefObject,
+    ReactNode,
+    createContext,
+    useEffect,
+    useRef,
+    useState } from "react";
 
 type Props = {
     children: ReactNode;
 }
 
 type AppContextType = {
-    deliveryMethodModalIsOpen: boolean,
+    deliveryMethodModalIsOpen: boolean;
+    shoppingBagIsOpen: boolean;
     handleOpenDeliveryMethodModal: () => void;
     handleCloseDeliveryMethodModal: () => void;
+    handleToggleShoppingBag: () => void;
     isStickyVisible: boolean;
     ref: MutableRefObject<HTMLDivElement | null>;
     search: string;
     handleSearch: (e: ChangeEvent<HTMLInputElement>) => void;
+    bag: BagProduct[];
+    handleBag: (newItem: BagProduct[]) => void;
 }
 
 export const AppContext = createContext({} as AppContextType);
 
 export const AppContextProvider = ({ children }: Props) => {
-    const [deliveryMethodModalIsOpen, setDeliveryMethodModalIsOpen] = useState<boolean>(false);
+    const [deliveryMethodModalIsOpen,
+         setDeliveryMethodModalIsOpen] = useState<boolean>(false);
+    const [shoppingBagIsOpen, setShoppingBagIsOpen] = useState<boolean>(false);
     const [isStickyVisible, setIsStickyVisible] = useState<boolean>(false);
+    const [bag, setBag] = useState<BagProduct[]>([]);
     const [search, setSearch] = useState<string>('');
     const ref = useRef<HTMLDivElement | null>(null);
  
     const handleOpenDeliveryMethodModal = () => setDeliveryMethodModalIsOpen(true);
     const handleCloseDeliveryMethodModal = () => setDeliveryMethodModalIsOpen(false);
 
+    const handleToggleShoppingBag = () => setShoppingBagIsOpen(!shoppingBagIsOpen);
+
     const handleSearch = (e: ChangeEvent<HTMLInputElement>) => setSearch(e.target.value);
+
+    const handleBag = (items: BagProduct[]) => setBag(items);
     
     useEffect(() => {
         const handleScroll = () => {
@@ -46,12 +65,16 @@ export const AppContextProvider = ({ children }: Props) => {
     return (
         <AppContext.Provider value={{ 
                 deliveryMethodModalIsOpen, 
+                shoppingBagIsOpen,
                 handleOpenDeliveryMethodModal, 
                 handleCloseDeliveryMethodModal,
+                handleToggleShoppingBag,
                 isStickyVisible,
                 ref,
                 search,
-                handleSearch
+                handleSearch,
+                bag,
+                handleBag
             }}>
             {children}
         </AppContext.Provider>
