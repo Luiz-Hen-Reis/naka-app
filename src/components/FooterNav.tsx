cc335a5a182a260263f34e951d588af7839fec8f
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-25 12:13:29 -0300

    refactor: fix z-index value

--- src/components/FooterNav.tsx
+++ src/components/FooterNav.tsx
@@ -8,9 +8,9 @@ export default function FooterNav() {
   const { handleToggleShoppingBag } = useAppContext();
 
   return (
-    <footer className="fixed bottom-0 w-full bg-white pt-3 border-t-2 z-40 md:hidden">
+    <footer className="fixed bottom-0 w-full bg-white pt-3 border-t-2 md:hidden">
 
-      <div className={`flex justify-around items-center secondary-bg-color text-white py-4 text-xs mb-2 shadow-md ${bag.length > 0 ? '' : 'hidden'}`} onClick={handleToggleShoppingBag}>
+      <div className={`flex justify-around items-center secondary-bg-color text-white py-4 text-xs mb-2 shadow-md z-30 ${bag.length > 0 ? '' : 'hidden'}`} onClick={handleToggleShoppingBag}>
         <div className="relative">
           <img src="/assets/icons/bag_white.svg" alt="bag" />
           <span className="absolute top-0 right-0 w-3 h-3 rounded-full bg-[#EEEEEE] secondary-text-color flex items-center justify-center">{bag.length}</span>
@@ -19,7 +19,7 @@ export default function FooterNav() {
         <span>$ 0.00</span>
       </div>
 
-      <ul className="flex justify-evenly items-center">
+      <ul className="flex justify-evenly items-center z-40">
         {iconsArray.map((icon) => (
             <li className="w-5 cursor-pointer flex flex-col items-center" key={icon.iconName}>
                 <img src={icon.path} alt={icon.iconName} />
