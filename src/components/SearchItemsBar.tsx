4b3b5fc216110848e6c3271e04ad4db3800b2ff6
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-26 12:12:01 -0300

    refactor: implement deliveryMethod logic and add lucide-react icons

--- src/components/SearchItemsBar.tsx
+++ src/components/SearchItemsBar.tsx
@@ -1,20 +1,33 @@
 import { useAppContext } from "@/hooks";
 import DeliveryMethodModal from "./modals/DeliveryMethodModal";
 import { InputSearchItems } from "./ui";
+import { Bike, ChevronDown, PersonStanding } from "lucide-react";
 
 export default function SearchItemsBar() {
-    const { deliveryMethodModalIsOpen, handleOpenDeliveryMethodModal } = useAppContext();
+    const { deliveryMethodModalIsOpen, handleOpenDeliveryMethodModal, deliveryMethod } = useAppContext();
 
     return (
         <>
             <div className="mt-5 w-full flex flex-wrap gap-4 lg:gap-8 justify-between items-center">
                 <InputSearchItems autoFocus />
                 <div data-testid="delivery-method-button" className="flex items-center gap-2 cursor-pointer border-2 h-12 rounded-md lg:h-16 px-2 lg:w-36" onClick={handleOpenDeliveryMethodModal}>
-                    <span><img src="/assets/icons/delivery.svg" alt="takeaway icon" /></span>
-                    <button className="text-xs lg:text-sm flex items-center justify-center">
-                        Delivery
-                    </button>
-                    <span className="secondary-text-color">&#8744;</span>
+                    {deliveryMethod === 'Delivery' ? (
+                        <>
+                            <Bike color="#FF3E36" />
+                            <button className="text-xs lg:text-sm flex items-center justify-center">
+                                {deliveryMethod}
+                            </button>
+                        </>
+                    ) : (
+                        <>
+                            <PersonStanding color="#FF3E36" />
+                            <button className="text-xs lg:text-xs flex items-center justify-center">
+                                {deliveryMethod}
+                            </button>
+                        </>
+                    )
+                    }
+                    <ChevronDown color="#FF3E36" />
                 </div>
                 {deliveryMethodModalIsOpen &&
                     <DeliveryMethodModal />
