62bc1c769ebdb85b722765306bf711789922fa13
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-26 12:10:28 -0300

    type: create deliveryMethod type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,11 @@
 import { Product, Category, ApiData } from "./apiDataTypes";
 import { BagProduct } from "./bagProduct";
+import { IDeliveryMethod } from "./deliveryMethod";
 
 export type { 
     Product, 
     Category, 
     ApiData,
-    BagProduct
+    BagProduct,
+    IDeliveryMethod
  };
