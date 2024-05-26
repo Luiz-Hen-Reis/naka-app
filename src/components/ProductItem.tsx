3964618525cf5314e39cc873525d3ad2cdf85151
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-26 10:35:15 -0300

    style: fix image size

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -16,21 +16,29 @@ export default function ProductItem({ name, price, description, image_url, id }:
   const productProps = { id, name, price, image_url, description, onClose: handleCloseProductModal };
 
   return (
-      <>
-        <div className='flex justify-between items-start gap-2 py-4 md:px-4 md:border rounded-sm md:shadow-sm cursor-pointer hover:border-[#717171] transition' onClick={handleOpenProductModal}>
-          <div className='flex flex-col'>
-              <h3 className='font-bold pb-4 md:text-lg'>{name}</h3>
-              <p className='text-xs text-[#717171] pb-5'>{description}</p>
-              <span>$ {price}</span>
-          </div>
+    <>
+      <div 
+        className='flex justify-between items-start gap-2 py-4 md:px-4 md:border rounded-sm md:shadow-sm cursor-pointer hover:border-[#717171] transition' 
+        onClick={handleOpenProductModal}
+      >
+        <div className='flex flex-col'>
+          <h3 className='font-bold pb-4 md:text-lg'>{name}</h3>
+          <p className='text-xs text-[#717171] pb-5'>{description}</p>
+          <span>$ {price}</span>
+        </div>
 
-          <div>
-              <div className='w-24 h-16'>
-                  <img src={`${API_URI}${image_url}`} alt={name} className="object-cover" />
-              </div>
+        <div>
+          <div className='w-24 h-16'>
+            <img 
+              src={`${API_URI}${image_url}`} 
+              alt={name} 
+              className="object-cover w-full h-full" 
+              style={{ width: '96px', height: '64px' }}
+            />
           </div>
         </div>
-        {isProductModalOpen && <ProductModal {...productProps} />}
-      </>
-  )
+      </div>
+      {isProductModalOpen && <ProductModal {...productProps} />}
+    </>
+  );
 }
