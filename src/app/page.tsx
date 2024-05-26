e74b5bac1111f78230fc81ccb53794b9d26ee86d
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-26 10:09:50 -0300

    feat: create ShoppingBag component and add it to home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,19 @@ import {
   CategoryItemSkeleton, 
   MainHeader, 
   SearchItemsBar, 
+  ShoppingBag, 
   StickySearchBar
 } from "@/components";
 import { useAppContext, useGetProducts } from "@/hooks";
 
 export default function Home() {
   const { data, isLoading } = useGetProducts();
-  const { isStickyVisible, ref } = useAppContext();
+  const { isStickyVisible, ref, shoppingBagIsOpen } = useAppContext();
 
   return (
     <>
       <MainHeader />
+      { shoppingBagIsOpen && <ShoppingBag /> }
       <div className="app-container">
         <div ref={ref}></div>
         { !isStickyVisible ? (
---
+++ src/components/ShoppingBag.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useAppContext, useBag } from "@/hooks";
+import { Button, CloseButton, EmptyBag, PriceDetailRow } from "./ui";
+import BagProductItem from "./BagProductItem";
+
+export default function ShoppingBag() {
+    const { handleToggleShoppingBag } = useAppContext();
+    const { bag, subtotal, deliveryFee, total } = useBag();
+
+    return (
+        <div className="w-full h-full fixed z-40">
+            <div className="flex flex-col fixed w-full h-full z-50 bg-white md:shadow-lg md:right-0 md:w-[500px]">
+                <CloseButton onClick={handleToggleShoppingBag} />
+
+                <div className="flex-1 flex flex-col mx-8 md:mx-12 overflow-y-hidden">
+                    <div className="flex-1 flex flex-col items-center overflow-y-auto pb-24">
+                        {bag.length === 0 ? (
+                            <EmptyBag />
+                        ) : (
+                            <>
+                                {bag.map((product) => (
+                                    <BagProductItem key={product.id} {...product} />
+                                ))}
+                            </>
+                        )}
+
+                        <div className="flex flex-col space-y-4 mb-4 md:mb-28 justify-between w-full mt-6">
+                          <PriceDetailRow label="Subtotal" value={subtotal.toString()} />
+                          <PriceDetailRow label="Tasa de Envio" value={deliveryFee.toString()} />  
+                        </div>
+                        <div className="fixed bottom-0 bg-white z-40 w-96 md:w-[420px] flex flex-col"> 
+                          <PriceDetailRow label="Total" value={bag.length === 0 ? '0000,00' : total.toFixed(2).toString()} className="mx-8 md:mx-0 font-bold text-lg" />  
+                          <Button disabled={bag.length === 0} className="disabled:opacity-50">Continuar</Button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+}
