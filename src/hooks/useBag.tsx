49895be6b803f9cf122e7e983f8295697ffe1c9f
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-26 10:05:24 -0300

    refactor: add bag functionalities: add, remove and update products

--- src/hooks/useBag.tsx
+++ src/hooks/useBag.tsx
@@ -3,11 +3,15 @@
 import { BagProduct } from "@/types";
 import { useEffect, useState } from "react";
 import { useAppContext } from "./useAppContext";
+import { recoverItemsFromLocalStorage } from "@/services";
 
 export default function useBag() {
     const bagLocalStorageKey = 'bag-items';
 
     const { bag, handleBag } = useAppContext();
+    const subtotal = bag.reduce((sum, item) => sum += item.price * item.quantity, 0);
+    const deliveryFee = 1000.00;
+    const total = subtotal + deliveryFee;
   
     useEffect(() => {
         if (typeof window !== 'undefined') {
@@ -18,31 +22,48 @@ export default function useBag() {
         };
     }, []);
 
-    const updateLocalStorage = (newValue: BagProduct[]) => {
+    const updateBagAndLocalStorage = (newValue: BagProduct[]) => {
         handleBag(newValue);
         localStorage.setItem(bagLocalStorageKey, JSON.stringify(newValue));
-    }
+    };
 
     const addItemToBag = (product: BagProduct) => {
-        let bagItems: BagProduct[] = [];
-
-        const storedItems = localStorage.getItem(bagLocalStorageKey);
-
-        if (storedItems) {
-            bagItems = JSON.parse(storedItems);
-        }
+        let bagItems: BagProduct[] = recoverItemsFromLocalStorage(bagLocalStorageKey);
 
         bagItems.push(product);
-        localStorage.setItem(bagLocalStorageKey, JSON.stringify(bagItems));
-        handleBag(bagItems);
+        updateBagAndLocalStorage(bagItems);
+    };
+
+    const removeItemFromBag = (id: string) => {
+        let bagItems: BagProduct[] = recoverItemsFromLocalStorage(bagLocalStorageKey);
+
+        const newBagItems = bagItems.filter((item) => item.id !== id);
+        updateBagAndLocalStorage(newBagItems);
+    };
+
+    const updateItemQuantity = (newQuantity: number, id: string) => {
+        let bagItems: BagProduct[] = recoverItemsFromLocalStorage(bagLocalStorageKey);
+
+        const currentProductIndex = bagItems.findIndex((item) => item.id === id);
+
+        if (currentProductIndex !== -1) {
+            let updatedBagItems = bagItems.slice();
+            updatedBagItems[currentProductIndex].quantity = newQuantity;
+            updateBagAndLocalStorage(updatedBagItems);
+        }
     }
 
     const clearBagItemsFromLocalStorage = () => localStorage.removeItem(bagLocalStorageKey);
 
     return {
         bag,
-        updateLocalStorage,
+        deliveryFee,
+        total,
+        updateLocalStorage: updateBagAndLocalStorage,
         addItemToBag,
-        clearBagItemsFromLocalStorage
+        removeItemFromBag,
+        updateItemQuantity,
+        clearBagItemsFromLocalStorage,
+        subtotal
     }
 }
