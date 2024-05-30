0bf0d67ad6a9570779b14fcda0b08fccab6b1cc8
Luiz Henrique <lhcontato2020@gmail.com>
2024-05-30 14:32:20 -0300

    spread props in li element

--- src/components/ui/FooterIcon.tsx
+++ src/components/ui/FooterIcon.tsx
@@ -1,14 +1,14 @@
 import { LucideProps } from "lucide-react";
 
-
-interface FooterIconProps {
+interface FooterIconProps extends React.HTMLAttributes<HTMLLIElement> {
     icon: React.ComponentType<LucideProps>;
     label: string;
 }
 
-export default function FooterIcon({ icon: Icon, label }: FooterIconProps) {
+export default function FooterIcon({ icon: Icon, label, ...props }: FooterIconProps) {
+
   return (
-    <li className="w-5 cursor-pointer flex flex-col items-center">
+    <li className="w-5 cursor-pointer flex flex-col items-center" {...props}>
         <Icon />
         <p className="font-extralight text-sm">{label}</p>
     </li>
